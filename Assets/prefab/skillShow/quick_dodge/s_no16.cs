136398719df8e3a5df032f2cf036626f68f2404b
kiam123 <kiam125@gmail.com>
2018-04-01 12:51:52 +0800

    finish quick dodge

---
+++ Assets/prefab/skillShow/quick_dodge/s_no16.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_no16 : Skill
+{
+    public override void trigger(object traget)
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
